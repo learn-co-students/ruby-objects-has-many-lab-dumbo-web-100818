6c364bdd13a9656a545f7d93f498177d2783a2dd
github username <github email address>
2018-10-11 22:07:08 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,18 +9,30 @@ class Song
   end
 
   def self.all
-    @@all        #calls @@all 
+    @@all        #calls @@all
   end
 
-
-
- def add_song(song)
-   song.artist = self
-   @song << song
- end
+  def add_song(song)
+    song.artist = self
+    @song << song
+  end
 
  def songs
    @songs = []
  end
 
+ def artist_name
+   if self.artist.name == ""    #self is a song, pool the artist name
+     return nil
+   else
+     return self.artist.name
+ end
+
+
 end
+
+
+it 'knows the name of its artist' do
+  drake = Artist.new('Drake')
+  song.artist = drake
+  expect(song.artist_name).to eq('Drake')
